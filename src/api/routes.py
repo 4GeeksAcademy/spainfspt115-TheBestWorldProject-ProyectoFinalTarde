3b3af31d417aa1051ea27b8d6fd10a74a4bea1c4
TrivmviraT <consvpr@outlook.es>
2025-09-01 23:59:15 +0200

    fix:rutas actualizacion

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,14 +8,41 @@ from api.models.modelUser import User
 from api.extensions import db
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from datetime import datetime
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
-#Creación del usuario basado en el modelUser pa mi gente 
+@api.route('/user', methods =['GET'])
+def get_users():
+    users = User.query.all()
+    result = [user.serialize() for user in users]
+    print(result)
+    return jsonify(result)
+
+@api.route('/user/<int:user_id>', methods = ['GET'])
+def get_user(user_id):
+    user = user.query.get(user_id)
+    if user is None:
+        return jsonify({"error":"Usuario no existe"}),400
+    return jsonify(user.serialize()), 200
+
+
+
+@api.route('/user', methods =['GET'])
+def get_users():
+    users = User.query.all()
+    result = [user.serialize() for user in users]
+    return jsonify(result)
+
+@api.route('/user/<int:user_id>', methods = ['GET'])
+def get_user(user_id):
+    user = user.query.get(user_id)
+    if user is None:
+        return jsonify({"error":"Usuario no existe"}),400
+    return jsonify(user.serialize()), 200
+
 @api.route('/user', methods=['POST'])
 def create_user():
     data = request.get_json()
@@ -58,6 +85,8 @@ def update_user(user_id):
     user.last_update = datetime.now()
     db.session.commit()
     return jsonify(user.serialize()), 200
+
+
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
 
@@ -66,3 +95,53 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/game', methods = ['GET'])
+def get_games():
+    games = Game.query.all()
+    result = [game.serialize() for game in games]
+    return jsonify(result), 200
+
+@api.route('/game/<int:id_game>', methods = ['GET'])
+def get_game(id_game):
+    game = game.query.get(id_game)
+    if game is None:
+        return jsonify({"error":"Usuario no existe"}),400
+    return jsonify(game.serialize()), 200
+
+@api.route('/game', methods = ['POST'])
+def create_game():
+    body = request.get_json()
+
+    id_user =body.get("id_user")
+    final_score = body.get("final_score")
+    correct_words = body.get("correct_words")
+    failed_words = body.get("failed_words")
+    average_precision = body.get("average_precision")
+    wpm_average = body.get("wpm_average")
+    difficulty = body.get("difficulty")
+
+
+    if not id_user:
+          return jsonify({"error": "id_user es obligatorio"}), 400
+    
+    user = User.query.get(id_user)
+    if not user:
+        return jsonify({"error": "El usuario no existe"}), 400
+
+
+    game = Game (
+        id_user=id_user,
+        final_score=final_score,
+        correct_words=correct_words,
+        failed_words=failed_words,
+        average_precision=average_precision,
+        wpm_average=wpm_average,
+        difficulty=difficulty,
+    )
+
+
+    db.session.add(game)
+    db.session.commit()
+
+    return jsonify(game.serialize()), 201
