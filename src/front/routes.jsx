1bfbc3d92f8bf09d2cf21a35c507001562a5ea04
Jmvdev <jvi96miguel@gmail.com>
2025-09-02 16:48:01 +0000

    feat: comments added

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -26,8 +26,7 @@ export const router = createBrowserRouter(
       <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
 
         <Route path= "/" element={<Home />} />
-        <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
-        {/*<Route path="/demo" element={<Demo />} />*/} 
+        <Route path="/single/:theId" element={ <Single />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/single/:theId" element={ <Single />} />
         <Route path="/demo" element={<Demo />} />
