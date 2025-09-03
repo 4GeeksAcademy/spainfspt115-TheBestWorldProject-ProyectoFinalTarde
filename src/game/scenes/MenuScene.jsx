73365c546a01be662b4bdc06c29f6feb6934049b
Jmvdev <jvi96miguel@gmail.com>
2025-09-03 18:57:39 +0000

    feat: comments

--- src/game/scenes/MenuScene.jsx
+++ src/game/scenes/MenuScene.jsx
@@ -6,10 +6,17 @@ export default class MenuScene extends Phaser.Scene {
   }
 
   create() {
+    //Boton de inicio de partida
     const startButton = this.add.image(400, 300, "buttonImg").setInteractive();
 
     startButton.on("pointerdown", () => {
       this.scene.start("GameScene");
     });
+
+    //Boton para mostrar tabla score
+
+    //Boton para ir a la scene ajustes
+
+    //Boton salir
   }
 }
